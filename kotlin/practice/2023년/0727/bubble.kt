fe4d6cb8c3cfc92f2e07d71b32a36b6900052d4c
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-07-27 18:13:17 +0900

    gpt의 도움을 받아 조건을 넣어 쓸데없는반복동작을 진행하면 break 하게 만들었다.

--- kotlin/practice/2023년/0727/bubble.kt
+++ kotlin/practice/2023년/0727/bubble.kt
@@ -14,36 +14,54 @@
 
 fun upbubble(input: Array<Int>) {
     for (i in 0 until input.size) {
-        for (indexNum in 0 until input.size) {
-            val temp = input[indexNum]
+        //gpt의 도움을 받음 swap이라는 boolean변수로 밑에 있는 if문에서 자료순서가 바뀐 부분이 있을때
+        //true로 바꿔 정렬을 다했을때 불필요하게 반복을 하는 작업이 일어나면 break를 하게 만들었다.
+        var isSwap = false
+
+        //gpt의 도음을 받음 input.size-i 이부분은 마지막 값은 정렬이 된 상태이기 때문에 볼 필요가 없어서
+        for (indexNum in 0 until input.size - i) {
 
             if (indexNum > 0 && input[indexNum] < input[indexNum - 1]) {
+                //gpt의 도움을 받음 temp값은 값을 바꿀때만 생성함
+                val temp = input[indexNum]
                 input[indexNum] = input[indexNum - 1]
                 input[indexNum - 1] = temp
+                isSwap = true
             }
 //            println(input.contentToString())
 
         }
-
+        //gpt의 도움을 받음 이렇게 되면 마지막까지 정렬이 되었다면 두번째 for문을 돌지 않기 때문에
+        //isSwap이 false그대로 가서 break를 해 정렬탐색을 중지하며 결과가 나온다.
+        if (!isSwap) {
+            break
+        }
     }
     println(input.contentToString())
+
 }
 
 upbubble(arrayOf(7, 2, 8, 10, 3, 1, -3, 4))
 
 fun downbubble(input: Array<Int>) {
     for (i in 0 until input.size) {
-        for (indexNum in 0 until input.size) {
-            val temp = input[indexNum]
+        var isSwap = false
+
+        for (indexNum in 0 until input.size - i) {
 
             if (indexNum > 0 && input[indexNum] > input[indexNum - 1]) {
+                val temp = input[indexNum]
                 input[indexNum] = input[indexNum - 1]
                 input[indexNum - 1] = temp
+                isSwap = true
             }
 //            println(input.contentToString())
 
         }
 
+        if (!isSwap) {
+            break
+        }
     }
     println(input.contentToString())
 
@@ -67,7 +85,6 @@ downbubble(arrayOf(7, 2, 8, 10, 3, 1, -3, 4))
 [8, 10, 7, 3, 2, 1, 4, -3]
 [8, 10, 7, 3, 2, 4, 1, -3]
 [8, 10, 7, 3, 2, 4, 1, -3]
-[8, 10, 7, 3, 2, 4, 1, -3]
 [10, 8, 7, 3, 2, 4, 1, -3]
 [10, 8, 7, 3, 2, 4, 1, -3]
 [10, 8, 7, 3, 2, 4, 1, -3]
@@ -77,39 +94,6 @@ downbubble(arrayOf(7, 2, 8, 10, 3, 1, -3, 4))
 [10, 8, 7, 3, 4, 2, 1, -3]
 [10, 8, 7, 3, 4, 2, 1, -3]
 [10, 8, 7, 3, 4, 2, 1, -3]
-[10, 8, 7, 3, 4, 2, 1, -3]
-[10, 8, 7, 3, 4, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3] == 여기?서 끝나야 됨
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
-[10, 8, 7, 4, 3, 2, 1, -3]
 [10, 8, 7, 4, 3, 2, 1, -3]
 [10, 8, 7, 4, 3, 2, 1, -3]
 [10, 8, 7, 4, 3, 2, 1, -3]
