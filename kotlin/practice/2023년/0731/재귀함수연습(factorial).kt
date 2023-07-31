219fbe585f6fcc9e0cde3756f68e90426dd00105
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-07-31 17:16:24 +0900

    Update 재귀함수연습(factorial).kt

--- kotlin/practice/2023년/0731/재귀함수연습(factorial).kt
+++ kotlin/practice/2023년/0731/재귀함수연습(factorial).kt
@@ -1,1 +1,13 @@
-
+//자 팩토리얼
+//펙토리얼 이란
+//!이다
+//느낌표는
+//n!은 n*(n-1)*(n-2)*...*1 이다
+//자
+fun factorial(n: Int): Int {
+    if (n == 1) {
+        return 1
+    }
+    return n * factorial(n - 1)
+}
+factorial(10)
