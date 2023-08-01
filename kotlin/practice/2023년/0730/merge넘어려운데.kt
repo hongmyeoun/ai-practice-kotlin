5b629b9e05e4bd39765e2c358220b3c140b87955
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-08-01 15:21:56 +0900

    mergeSort 구현과 이해

--- kotlin/practice/2023년/0730/merge넘어려운데.kt
+++ kotlin/practice/2023년/0730/merge넘어려운데.kt
@@ -8,14 +8,12 @@
 //나머지는 rightlist가됨
 //요소가 하나가 될 때 까지 반복
 //다 나눴다.
-
 //이런식으로 리스트들을 병합
 //마지막 list를 출력
 
 
-val list = mutableListOf<Int>(10, 4, 2, 1, 7, 3, 13, 5)
 //list를 2로 나눈값까지 새 리스트에 저장
-fun listdivider(input: List<Int>): List<Int>{
+fun listdivider(input: List<Int>): List<Int> {
     if (input.size < 2) {
         return input
     }
@@ -32,14 +30,14 @@ fun listdivider(input: List<Int>): List<Int>{
         }
     }
     //1. 10,4,2,1
-    val leftDivided = leftList
+    val leftDivided = listdivider(leftList)
     //1. 7,3,13,5
-    val rightDivided = rightList
+    val rightDivided = listdivider(rightList)
     //한번 나눈 list를 병합
-    println("$leftDivided $rightDivided")
-    return merge(leftDivided, rightDivided)
+//    println("$leftDivided $rightDivided")
+    return upmerge(leftDivided, rightDivided)
 }
-println(listdivider(list))
+
 //리스트를 합치기 전에
 //0번과 1번의 크기를 비교
 //크기순으로 크기가 2인 리스트에 추가
@@ -50,9 +48,7 @@ println(listdivider(list))
 //남은거 비교 작은거 list3에 2번에 저장
 //남은거 list3에 3번에 저장
 //나머지 반복
-
-//println(merge(listOf(10,4),listOf(2,1)))
-fun merge(left: List<Int>, right: List<Int>): List<Int> {
+fun upmerge(left: List<Int>, right: List<Int>): List<Int> {
     //왼쪽의 0번째와 오른쪽의 0번째 크기비교
     //작은쪽이 새로운 정렬된 리스트에 먼저 저장됨
     //2개 2개일때
@@ -73,13 +69,74 @@ fun merge(left: List<Int>, right: List<Int>): List<Int> {
     var leftIndex = 0
     var rightIndex = 0
 
-    //left[10,4,2,1] right[7,3,13,5]
-
-    while (leftIndex < left.size && rightIndex < right.size){
-        if(left[leftIndex] < right[rightIndex]){
+    //1. left[10] right[4]
+    //1. true && true
+    while (leftIndex < left.size && rightIndex < right.size) {
+        //1. false
+        if (left[leftIndex] < right[rightIndex]) {
             mergeSortList.add(left[leftIndex])
             leftIndex++
-        }else{
+        } else {
+            //1. mergeSortList[4]
+            mergeSortList.add(right[rightIndex])
+            rightIndex++
+        }
+    }
+    //남아있는 leftList의 값들을 추가한다.
+    //1. 0 < 1 true
+    while (leftIndex < left.size) {
+        //1. mergeSortList[4, 10], false
+        mergeSortList.add(left[leftIndex])
+        leftIndex++
+    }
+    //남아있는 rightList 값들을 추가한다
+    //1. 1 < 1 false
+    while (rightIndex < right.size) {
+        //1. mergeSortList[4,10]
+        mergeSortList.add(right[rightIndex])
+        rightIndex++
+    }
+
+    return mergeSortList
+}
+
+val list = mutableListOf<Int>(10, 4, 2, 1, 7, 3, 13, 5)
+println(listdivider(list))
+
+fun listdivider2(input: List<Int>): List<Int> {
+    if (input.size < 2) {
+        return input
+    }
+    val mid = input.size / 2
+    val leftList = mutableListOf<Int>()
+    val rightList = mutableListOf<Int>()
+    for (i in 0 until input.size) {
+        if (i < mid) {
+            leftList.add(input[i])
+        } else {
+            rightList.add(input[i])
+        }
+    }
+    val leftDivided = listdivider2(leftList)
+    val rightDivided = listdivider2(rightList)
+//    println("$leftDivided $rightDivided")
+    return downmerge(leftDivided, rightDivided)
+}
+
+fun downmerge(left: List<Int>, right: List<Int>): List<Int> {
+
+    val mergeSortList = mutableListOf<Int>()
+    var leftIndex = 0
+    var rightIndex = 0
+
+
+    while (leftIndex < left.size && rightIndex < right.size) {
+        //왼쪽이 더 크면 왼쪽을 추가
+        if (left[leftIndex] > right[rightIndex]) {
+            mergeSortList.add(left[leftIndex])
+            leftIndex++
+        } else {
+            //아니면 오른쪽 추가
             mergeSortList.add(right[rightIndex])
             rightIndex++
         }
@@ -97,3 +154,4 @@ fun merge(left: List<Int>, right: List<Int>): List<Int> {
 
     return mergeSortList
 }
+println(listdivider2(mutableListOf<Int>(10, 4, 2, 1, 7, 3, 13, 5)))
