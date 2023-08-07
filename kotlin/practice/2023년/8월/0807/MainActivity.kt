a23893d55f1927c438b7c911aa571fcba613c4ab
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-08-07 19:29:04 +0900

    Update MainActivity.kt

--- kotlin/practice/2023년/8월/0807/MainActivity.kt
+++ kotlin/practice/2023년/8월/0807/MainActivity.kt
@@ -20,12 +20,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,6 +46,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -67,7 +72,7 @@ class MainActivity : ComponentActivity() {
                                 context.startActivity(intent)
                             },
                             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                            ) {
+                        ) {
                             Text("➕")
                         }
                     }
@@ -86,9 +91,21 @@ class MainActivity : ComponentActivity() {
                                         intent.putExtra("UserName", user.name)
                                         intent.putExtra("UserPhone", user.phone)
                                         intent.putExtra("UserEmail", user.email)
+                                        intent.putExtra("UID", user.uid)
                                         context.startActivity(intent)
                                     }
                             ) {
+                                Column(){
+                                    var newName by remember{ mutableStateOf(user.name!!)}
+                                    TextField(value = newName, onValueChange = {newName = it})
+                                    Button(onClick = {
+                                        scope.launch(Dispatchers.IO) {
+                                            user.name = newName
+                                            db.userDao().updateUser(user) }
+                                    }) {
+                                        Text("update")
+                                    }
+                                }
                                 Row(verticalAlignment = Alignment.CenterVertically) {
                                     UserItem(user, db, scope)
                                 }
