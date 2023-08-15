44fae844591260fedf43aa959ea19472e731a1b4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-08-15 22:35:20 +0900

    사용방법

--- kotlin/practice/2023년/프로젝트/폰트넣기.kt
+++ kotlin/practice/2023년/프로젝트/폰트넣기.kt
@@ -2,3 +2,14 @@
 
 일단 폰트 이름은 소문자영어와 언더바로 이루어져있어야한다.
 
+1. res파일에 우클릭
+2. new
+3. Directory -> font 입력
+4. 폰트파일을 방금만든 font에 드래그앤드롭
+5. font 우클릭
+6. new
+7. Font Resource File
+8. File name  font 입력
+9. font.xml 들어가기
+10. <font android:font="@font/폰트이름"/> font-family 사이에 넣기
+11. fontFamily = FontFamily(Font(R.font.폰트이름)) 으로 사용하기
